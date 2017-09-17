a9b85393a0f77f3ddd8ad5a238ba6d8429ddc48c
Adam Straw <adam.d.straw@intel.com>
2017-09-16 17:53:47 -0700

    single variable linear regression

--- machine-learning-ex1/ex1/gradientDescent.m
+++ machine-learning-ex1/ex1/gradientDescent.m
@@ -16,7 +16,8 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
-
+    sum = X * theta - y; % mx2 * 2x1 = mx1 - mx1 = mx1
+    theta = theta - (alpha/m) * X' * sum; % 2xm * mx1 = 2x1
 
 
 
