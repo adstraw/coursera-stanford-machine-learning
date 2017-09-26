2c16a6bb1839477ec84a852e9977211877b1ed3c
Adam Straw <adam.d.straw@intel.com>
2017-09-25 21:48:25 -0700

    backprop

--- machine-learning-ex4/ex4/nnCostFunction.m
+++ machine-learning-ex4/ex4/nnCostFunction.m
@@ -38,7 +38,24 @@ Theta2_grad = zeros(size(Theta2));
 %         variable J. After implementing Part 1, you can verify that your
 %         cost function computation is correct by verifying the cost
 %         computed in ex4.m
-%
+
+% X = 5000x400
+% Theta1 = 25x401
+% Theta2 = 10x26
+
+% forward prop
+a1b = [ones(m, 1) X]; % 5000x401
+a2 = sigmoid(a1b * Theta1'); % 5000x401 * 401x25 = 5000x25
+a2b = [ones(m, 1) a2]; % 5000x26
+a3 = sigmoid(a2b * Theta2'); % 5000x26 * 26x10 = 5000x10
+
+% convert y from labels vector to binary matrix
+yd = eye(num_labels);
+y = yd(y,:); % 5000x10
+
+% non-regularized cost
+J = sum(sum((-y .* log(a3) - (1 - y) .* log(1 - a3)))) / m;
+
 % Part 2: Implement the backpropagation algorithm to compute the gradients
 %         Theta1_grad and Theta2_grad. You should return the partial derivatives of
 %         the cost function with respect to Theta1 and Theta2 in Theta1_grad and
@@ -53,7 +70,19 @@ Theta2_grad = zeros(size(Theta2));
 %         Hint: We recommend implementing backpropagation using a for-loop
 %               over the training examples if you are implementing it for the 
 %               first time.
-%
+
+% trim off bias for backprop
+Theta1z = Theta1(:, 2:end); % 25x400
+Theta2z = Theta2(:, 2:end); % 10x25
+
+% back prop
+d3 = a3 .- y; % 5000x10
+d2 = d3 * Theta2z .* a2 .* (1 - a2); % 5000x10 * 10x25 = 5000x25
+
+% gradient
+Theta1_grad = (d2' * a1b) / m; % 25x5000 * 5000x401 = 25x401
+Theta2_grad = (d3' * a2b) / m; % 10x5000 * 5000x26 = 10x26
+
 % Part 3: Implement regularization with the cost function and gradients.
 %
 %         Hint: You can implement this around the code for
@@ -62,23 +91,16 @@ Theta2_grad = zeros(size(Theta2));
 %               and Theta2_grad from Part 2.
 %
 
+% disregard bias for regularization
+Theta1(:,1) = 0;
+Theta2(:,1) = 0;
 
+% regularized cost
+J = J + (lambda / (2 * m)) * (sum(sum(Theta1.^2)) +  sum(sum(Theta2.^2)));
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+% regularized gradient
+Theta1_grad = Theta1_grad + (lambda / m) .* Theta1;
+Theta2_grad = Theta2_grad + (lambda / m) .* Theta2;
 
 % -------------------------------------------------------------
 
