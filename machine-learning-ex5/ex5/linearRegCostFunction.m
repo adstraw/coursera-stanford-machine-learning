3d5eb0e53a3a31159fa85b3ff2dd6c7173a61f69
Adam Straw <adam.d.straw@intel.com>
2017-09-26 18:56:59 -0700

    regularized linear regression with learning curve

--- machine-learning-ex5/ex5/linearRegCostFunction.m
+++ machine-learning-ex5/ex5/linearRegCostFunction.m
@@ -9,8 +9,10 @@ function [J, grad] = linearRegCostFunction(X, y, theta, lambda)
 m = length(y); % number of training examples
 
 % You need to return the following variables correctly 
-J = 0;
-grad = zeros(size(theta));
+h = X * theta;
+theta(1) = 0;
+J = (1 / (2 * m)) * sum((h - y) .^ 2) + (lambda / (2 * m)) * sum(theta .^ 2);
+grad = (1 / m) * (X' * (h - y)) + (lambda / m) * theta;
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost and gradient of regularized linear 
